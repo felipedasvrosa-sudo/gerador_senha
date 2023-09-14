f788f6329185e2a2699e7b3acd056175c897fc4f
felipedasvrosa-sudo <felipedasvrosa.profissional@gmail.com>
2023-09-14 15:30:49 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+
+letra_maiuscula = chr(random.randint(65, 90))
+letra_minuscula = chr(random.randint(97, 122))
+char_especial = chr(33)
+lista_numeros = []
+
+for i in range(7):
+    numeros = random.randrange(9)
+    lista_numeros.append(numeros)
+random.shuffle(lista_numeros)
+lista_numeros = str(lista_numeros) [1:-1]
+lista_numeros = lista_numeros.replace(',','')
+
+print(letra_maiuscula, letra_minuscula, lista_numeros, char_especial)
